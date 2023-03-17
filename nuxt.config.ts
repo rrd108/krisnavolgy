7aa1ee3ea57536251fc788df330ceb5738e53420
rrd108 <rrd@webmania.cc>
2023-03-17 16:48:25 +0100

    nuxt devtools added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/strapi', '@vueuse/nuxt'],
+  modules: ['@nuxt/devtools', '@nuxtjs/strapi', '@vueuse/nuxt'],
   strapi: {
     url: process.env.STRAPI_URL || 'http://127.0.0.1:1337',
   },

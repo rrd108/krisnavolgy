17d2e03e474e0e9baa7e95e2e4b2d6c7c087f259
Sukhayanti dd <sk@krisna.hu>
2023-03-24 10:29:52 +0100

    gift card page done

---
+++ backend/src/api/gift-card-page/controllers/gift-card-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gift-card-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::gift-card-page.gift-card-page');

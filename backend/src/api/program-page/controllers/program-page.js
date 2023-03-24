00b502e36cace2a9a059299ce80a5620282a1892
Sukhayanti dd <sk@krisna.hu>
2023-03-24 11:35:56 +0100

    Program page created

---
+++ backend/src/api/program-page/controllers/program-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * program-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::program-page.program-page');

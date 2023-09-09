a250a15151fcea908d5a186c8173ba32da8d907b
thomas <git@mlndks.nl>
2023-09-09 11:03:42 +0200

    installed navigation plugin

--- backend/src/api/nav-bar/routes/nav-bar.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * nav-bar router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::nav-bar.nav-bar');
--- backend/src/api/nav-bar/services/nav-bar.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * nav-bar service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::nav-bar.nav-bar');

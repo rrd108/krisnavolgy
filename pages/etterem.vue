c2983703e5334c8f0ad0d1401ba582ebd97a9c7b
rrd108 <rrd@webmania.cc>
2024-10-26 11:55:58 +0200

    feat add restaurant page

--- pages/etterem.vue
+++ pages/etterem.vue
@@ -1,7 +1,40 @@
 <script setup></script>
 
 <template>
-  <h1><Icon name="ic:sharp-restaurant-menu" /> Étterem</h1>
+  <h1><Icon name="ic:sharp-restaurant-menu" /> Govinda Étterem</h1>
+
+  <p>
+    Fenséges, ajurvédikus, <strong>vegán és vegetáriánus</strong> ebéd, italok,
+    desszertek. A menü szezonális, vegyszermentes Krisna-völgyi zöldségek és
+    gabonafélék felhasználásával készül tele valódi vitaminokkal és ásványi
+    anyagokkal. Különlegesség a helyi tehenészetből származó ahimsa
+    (erőszakmentes) és tejtermékek megjelenése a napi kínálatunkban.
+  </p>
+
+  <Divider />
+
+  <h3>Nyitvatartás</h3>
+  <ul>
+    <li>
+      <Icon name="material-symbols:schedule" /> május 1. - szeptember 30.
+      között: kedd - vasárnap, 12:30 - 16:00
+    </li>
+    <li>
+      <Icon name="material-symbols:schedule" /> október 1. - április 30. között:
+      szombat, vasárnap 12:30 - 16:00, hétköznap csoportoknak előzetes egyeztetés szerint!
+    </li>
+  </ul>
+
+  <Divider />
+
+  <h3>Fizetés</h3>
+  <ul>
+    <li><Icon name="mdi:cash" /> készpénz</li>
+    <li><Icon name="f7:creditcard" /> bankkártya</li>
+    <li><Icon name="solar:card-recive-broken" /> SZÉP kártya</li>
+  </ul>
+
+  <p>Kóstolj bele Te is Krisna-völgyi terményeink ízvarázsába!</p>
 </template>
 
 <style scoped></style>

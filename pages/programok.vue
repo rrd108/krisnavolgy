3d66a15a0981cc6a9a5adbcb78e25ad013bbf432
rrd108 <rrd@webmania.cc>
2024-10-26 11:49:51 +0200

    feat add programok for #67

--- pages/programok.vue
+++ pages/programok.vue
@@ -1,9 +1,20 @@
-<script setup></script>
+<script setup>
+import { touristComingEvents } from '~/data/events';
+</script>
 
 <template>
-  <h1>
-    <Icon name="material-symbols:tips-and-updates-outline-rounded" /> Programok
-  </h1>
+  <h1><Icon name="material-symbols:tips-and-updates-outline-rounded" /> Programok</h1>
+
+  <div v-for="event in touristComingEvents">
+    <h2>{{ event.title }}</h2>
+    <h3>
+      {{ event.start_date }}
+      <span v-if="event.end_date !== event.start_date">
+        - {{ event.end_date }}
+        </span>
+    </h3>
+    <p>{{ event.description }}</p>
+  </div>
 </template>
 
 <style scoped></style>

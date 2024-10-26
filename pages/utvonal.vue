d3a7289b75e131d4083f04940e57569e23d91316
rrd108 <rrd@webmania.cc>
2024-10-26 12:18:16 +0200

    feat add path for #67

--- pages/utvonal.vue
+++ pages/utvonal.vue
@@ -2,6 +2,43 @@
 
 <template>
   <h1><Icon name="material-symbols-light:directions-car" /> Útvonal</h1>
+
+  <h3>Kaposvár felől</h3>
+  <NuxtLink
+    to="https://www.google.com/maps/dir/Kaposv%C3%A1r/Somogyv%C3%A1mos,+Krisna-v%C3%B6lgy"
+  >
+    <NuxtImg
+      src="/images/megkozelites-kaposvar-felol.jpg"
+      alt="Kaposvár felől"
+      sizes="sm:100vw md:100vw lg:100vw"
+      :modifiers="{ format: 'webp' }"
+      />
+    </NuxtLink>
+    <p>
+      Jután át Fonyód irányában, Somogyváron kell letérni Somogyvámosra.
+    (Kaposvártól 35 km)
+  </p>
+
+  <InfoBlock>
+    Több útvonal tervező javaslata ellenére <strong>Polányon keresztül</strong>
+    nem vagyunk személyautóval megközelíthetőek!
+  </InfoBlock>
+
+  <h3>Budapest és Balaton felől</h3>
+  <NuxtLink
+    to="https://www.google.com/maps/dir/Budapest/Somogyv%C3%A1mos,+Krisna-v%C3%B6lgy"
+  >
+    <NuxtImg
+      src="/images/megkozelites-budapest-felol.jpg"
+      alt="Budapest és Balaton felől"
+      sizes="sm:100vw md:100vw lg:100vw"
+      :modifiers="{ format: 'webp' }"
+    />
+  </NuxtLink>
+  <p>
+    Fonyódon vagy Balatonbogláron át Lengyeltótin keresztül Kaposvár irányában,
+    Somogyváron kell letérni Somogyvámosra. (Fonyódtól 30 km)
+  </p>
 </template>
 
 <style scoped></style>

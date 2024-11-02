f806f19cd1be49eca92aef0d3ce56cacdb794ff6
rrd108 <rrd@webmania.cc>
2024-11-02 15:04:06 +0100

    fix magázódásról tegeződésre fix #73

--- pages/hazirend.vue
+++ pages/hazirend.vue
@@ -17,24 +17,24 @@
     <ul>
       <li>
         <Icon name="icon-park-outline:shorts" />
-        Kérjük, hogy megfelelő ruházatban látogassák meg Krisna-völgyet, kerülve
+        Kérjük, hogy megfelelő ruházatban látogassátok meg Krisna-völgyet, kerülve
         a nagyon kihívó öltözetet: miniszoknya, mélyen dekoltált ruha, stb.
       </li>
       <li>
         <Icon name="material-symbols:no-drinks" />
-        Kérjük, ne fogyasszanak alkoholt, mielőtt hozzánk érkeznek, mert ittas
+        Kérjük, ne fogyasszatok alkoholt, mielőtt hozzánk érkeztek, mert ittas
         személyeket nem áll módunkban beengedni.
       </li>
       <li>
         <Icon name="mdi:food-drumstick-off" />
-        Kérjük, ne hozzák magukkal húst, halat, tojást tartalmazó, illetve
+        Kérjük, ne hozzatok magatokkal húst, halat, tojást tartalmazó, illetve
         alkohol tartalmú ételt vagy italt, mert ilyen élelmiszereket nem lehet
         fogyasztani Krisna-völgyben.
       </li>
       <li>
         <Icon name="mdi:dog-side-off" />
-        Kérjük, ne hozzák magukkal házi kedvenceiket - kutyát, macskát, és egyéb
-        háziállatot.<br />
+        Kérjük, ne hozzatok magatokkal házi kedvenceiket - kutyát, macskát, és
+        egyéb háziállatot.<br />
         Krisna-völgyben a természetes élővilág megmentésén, visszatelepítésén
         dolgozunk. Arborétum létrehozásával számos védett madárfajnak
         teremtettünk megfelelő otthont, és ezért is nem engedjük be pl. a
@@ -42,22 +42,22 @@
       </li>
       <li>
         <Icon name="material-symbols:smoke-free" />
-        Kérjük, hogy Krisna-völgy területén ne dohányozzanak, erre a célra a
+        Kérjük, hogy Krisna-völgy területén ne dohányozzatok, erre a célra a
         főbejárat előtt van kijelölt hely.
       </li>
       <li>
         <Icon name="ph:person-simple-swim" />
-        Kérjük, hogy a tavakban ne fürödjenek.
+        Kérjük, hogy a tavakban ne fürödjetek.
       </li>
       <li>
         <Icon name="tabler:plant-2-off" />
-        Kérjük, hogy ne szemeteljenek a területen, az állatokat ne etessék, és a
-        virágokat, növényeket ne szedjék le.
+        Kérjük, hogy ne szemeteljetek a területen, az állatokat ne etessétek, és a
+        virágokat, növényeket ne szedjetek le.
       </li>
     </ul>
     <p>
-      Köszönjük a megértésüket és reméljük, hogy kellemes pillanatokat szerez
-      majd Önöknek a Krisna-völgyi látogatás!
+      Köszönjük a megértéseteket és reméljük, hogy kellemes pillanatokat szereztek
+      majd a Krisna-völgyi látogatással!
     </p>
   </section>
 </template>

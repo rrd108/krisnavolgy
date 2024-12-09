2351c340b8798736b1f68c161c5df3908795dd33
rrd108 <rrd@webmania.cc>
2024-12-09 15:42:03 +0100

    feat open/close nav

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,32 +1,40 @@
 <script setup lang="ts">
   const userStore = useUserStore()
+  const isNavOpen = ref(true)
 </script>
 <template>
   <div>
-    <nav v-if="userStore.token">
+    <nav v-if="userStore.token" :class="{ closed: !isNavOpen }">
+      <Icon
+        id="navToggle"
+        :name="
+          isNavOpen ? 'material-symbols:arrow-circle-left' : 'material-symbols:arrow-circle-right'
+        "
+        @click="isNavOpen = !isNavOpen"
+      />
       <ul>
         <li>
-          <NuxtLink to="/admin/festivals">
+          <NuxtLink to="/admin/festivals" title="Programok">
             <Icon name="material-symbols:tips-and-updates-outline-rounded" />
-            Programok
+            <span class="navText">Programok</span>
           </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/admin/gift-cards">
+          <NuxtLink to="/admin/gift-cards" title="Ajándékutalvány">
             <Icon name="material-symbols:featured-seasonal-and-gifts" />
-            Ajándékutalványok
+            <span class="navText">Ajándékutalvány</span>
           </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/admin/upload">
+          <NuxtLink to="/admin/upload" title="Feltöltés">
             <Icon name="material-symbols:upload" />
-            Feltöltés
+            <span class="navText">Feltöltés</span>
           </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/admin/gallery">
+          <NuxtLink to="/admin/gallery" title="Galéria">
             <Icon name="jam:pictures" />
-            Galéria
+            <span class="navText">Galéria</span>
           </NuxtLink>
         </li>
       </ul>
@@ -42,13 +50,33 @@
     display: flex;
     gap: 1em;
   }
+  #navToggle {
+    font-size: 3em;
+  }
   nav {
+    width: 12em;
     background-color: var(--light);
     padding: 1em;
     min-height: 100dvh;
+    transition: all 300ms ease-in-out;
   }
   li {
     margin: 1em 0;
+    white-space: nowrap;
+  }
+  li a {
+    display: flex;
+    align-items: center;
+    gap: 1em;
+  }
+  nav.closed {
+    width: 3em;
+  }
+  nav.closed .navText {
+    display: none;
+  }
+  a span:first-child {
+    font-size: 1.5em;
   }
   main {
     flex: 1;

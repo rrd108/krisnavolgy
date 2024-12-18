105444933fcae2c4c08d6866d381cd33354441fc
rrd108 <rrd@webmania.cc>
2024-12-18 17:23:15 +0100

    feat add new page

---
+++ pages/adatkezeles.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+  useHead({ title: 'Adatkezelés' })
+</script>
+
+<template>
+  <h1>Adatkezelés</h1>
+  <h2>Általános tudnivalók, feltételek</h2>
+  <p>
+    A weboldalon történő vásárlásra interneten, banki utalással és paypalon van lehetőség. A
+    weboldalon bárki vásárolhat. Ezáltal magára nézve kötelező érvényűnek elfogadta jelen
+    szabályzatot. A vásárló felelősséggel tartozik a megadott adatokért. Az elektronikus
+    szolgáltatások, valamint az információs társadalommal összefüggő szolgáltatások egyes
+    kérdéseiről szóló 2011.évi CVIII törvény 5.paragr. (2) bek. értelmében kötelezően tájékoztatunk,
+    hogy a szerződés nyelve: magyar.
+  </p>
+
+  <h2>Az vásárlás folyamata</h2>
+  <p>
+    A vásárlónak lehetősége van kiválasztani a terméket csomagot. Az vásárlási szándék esetén a
+    "Bankkártyás fizetés" gomb megnyomásával kezdeményezheted az vásárlást. Itt leellenőrizheted az
+    vásárlás részleteit.
+  </p>
+
+  <h2>Az vásárlás véglegesítése</h2>
+  <p>
+    A weboldalon történt vásárlással minden vásárló kijelenti, hogy elfogadja jelen szabályzatot, és
+    tisztában van a vásárlás menetével. Ha megfelelőnek tartod a megadott összeget és adatokat,
+    akkor egyszerűen kattints a "Bankkártyás vásárlás" gombra.
+  </p>
+
+  <h2>Adattovábbítási nyilatkozat</h2>
+  <p>
+    Tudomásul veszem, hogy a Vénu Kft (8699 Somogyvámos Fő u. 38.) adatkezelő által a krisnavolgy.hu
+    felhasználói adatbázisában tárolt alábbi személyes adataim átadásra kerülnek az OTP Mobil Kft.,
+    mint adatfeldolgozó részére.
+  </p>
+  <p>Az adatkezelő által továbbított adatok köre az alábbi</p>
+  <ul>
+    <li>Név</li>
+    <li>E-mail cím</li>
+    <li>Cím</li>
+  </ul>
+  <p>
+    Az adatfeldolgozó által végzett adatfeldolgozási tevékenység jellege és célja a SimplePay
+    Adatkezelési tájékoztatóban, az alábbi linken tekinthető meg:
+    <NuxtLink to="https://simplepay.hu/vasarlo-aff">SimplePay Adatkezelési tájékoztató</NuxtLink>
+  </p>
+</template>
+
+<style scoped></style>
